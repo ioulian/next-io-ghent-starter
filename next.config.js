cf2666665161a720f6ea467f4e1abbd2a0e1ede7
yulian.alexeyev <yulian.alexeyev@intracto.com>
2021-10-19 15:55:17 +0200

    add bundle analyser and fix eslint

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 const { i18n } = require("./next-i18next.config");
 const withPWA = require("next-pwa");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
-  reactStrictMode: true,
-  i18n,
-  pwa: {
-    dest: "public",
-    register: false,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    i18n,
+    pwa: {
+      dest: "public",
+      register: false,
+      disable: process.env.NODE_ENV === "development",
+    },
+  })
+);
