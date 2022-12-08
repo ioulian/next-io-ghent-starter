4454670b105b0515d9514f5a2cbf74fc790c0ad1
yulian.alexeyev <yulian.alexeyev@intracto.com>
2022-12-08 09:49:46 +0100

    add router utility

---
+++ src/utils/router.ts
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+
+export const useIsActiveRoute = (
+  path: string,
+  exact: boolean = false
+): boolean => {
+  const router = useRouter();
+
+  if (exact) {
+    return router.asPath === path;
+  }
+
+  return router.asPath.startsWith(path);
+};
