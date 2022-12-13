5527129a441971c1a03e75239c7a25f6da434942
yulian.alexeyev <yulian.alexeyev@intracto.com>
2022-12-13 09:26:51 +0100

    Tree shake color.js

--- src/components/common/easing-gradient/utilities.ts
+++ src/components/common/easing-gradient/utilities.ts
@@ -1,6 +1,12 @@
 import { easingCoordinates } from "easing-coordinates";
-import Color from "colorjs.io";
+import { mix, parse, ColorSpace, sRGB, Lab, serialize } from "colorjs.io/fn";
 
+// If you want to use more color spaces, you'll need to register them first
+ColorSpace.register(sRGB);
+ColorSpace.register(Lab);
+
+// Some info: https://non-boring-gradients.netlify.app/
+// "Ported" from https://github.com/larsenwork/postcss-easing-gradients
 export const createEasingGradient = (
   from: string,
   to: string,
@@ -8,13 +14,18 @@ export const createEasingGradient = (
   stops: number = 16
 ) => {
   const coordinates = easingCoordinates(easing, stops);
+
+  // TS ignore is there because the types for serialize are not correct. This works.
   return coordinates
     .map(
       ({ x, y }) =>
-        `${Color.mix(from, to, y, {
-          space: "lab",
-          outputSpace: "srgb",
-        })} ${Math.abs(x * 100).toFixed(2)}%`
+        `${serialize(
+          // @ts-ignore
+          mix(parse(from), parse(to), y, {
+            space: "lab",
+            outputSpace: "srgb",
+          })
+        )} ${Math.abs(x * 100).toFixed(2)}%`
     )
     .join(", ");
 };
