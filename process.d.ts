3bc0670ef184c4dcd3bc740645480ef77b83f214
yulian.alexeyev <yulian.alexeyev@intracto.com>
2023-01-13 09:20:52 +0100

    remove unused .env var

--- process.d.ts
+++ process.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_SITE_URL: string;
     NEXT_PUBLIC_API_DOMAIN: string;
-    NEXT_PUBLIC_HEAD_TITLE: string;
     NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID?: string;
     NEXT_PUBLIC_AUTH_COOKIE_MAX_AGE: string;
     NEXT_PUBLIC_AUTH_LOGIN_URL: string;
