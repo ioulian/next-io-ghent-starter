326d456aff7b52816ff4e6684c2463dd1d33893f
yulian.alexeyev <yulian.alexeyev@intracto.com>
2023-05-25 14:41:51 +0200

    remove commented code

--- src/@types/styled.ts
+++ src/@types/styled.ts
@@ -1,17 +1,5 @@
 import { ComponentPropsWithRef, JSXElementConstructor } from "react";
-// import {
-//   AnyStyledComponent,
-//   StyledComponentInnerComponent,
-//   StyledComponentInnerOtherProps,
-// } from "styled-components";
-//
-// export type InferComponentProps<T> = T extends AnyStyledComponent
-//   ? ComponentPropsWithRef<StyledComponentInnerComponent<T>> &
-//       StyledComponentInnerOtherProps<T>
-//   : // eslint-disable-next-line no-undef
-//   T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>
-//   ? ComponentPropsWithRef<T>
-//   : never;
+
 export type InferComponentProps<T> = T extends
   | keyof JSX.IntrinsicElements
   | JSXElementConstructor<any>
